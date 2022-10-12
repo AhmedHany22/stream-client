8155f272d1dac7a1e93275a65d122df8bedfd188
Ahmed Hany <ahmed_hany19@outlook.com>
2022-10-12 14:08:59 +0200

    update show

--- src/Components/Streams/StreamShow/index.jsx
+++ src/Components/Streams/StreamShow/index.jsx
@@ -1,49 +1,14 @@
-import { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, connect } from "react-redux";
-import flv from "flv.js";
 
 import { fetchStream } from "./../../../Redux/Stream/Stream_ActionCreator";
-import Spinner from "../../Spinner";
 
 const StreamShow = (props) => {
   let { id } = useParams();
   const stream = useSelector((state) => state.stream[id]);
 
-  const videoRef = useRef();
-  let player = null;
-
-  const buildPlayer = () => {
-    if (stream) {
-      player = flv.createPlayer({
-        type: "flv",
-        url: `http://localhost:8000/live/${stream.id}.flv`,
-      });
-      player.attachMediaElement(videoRef.current);
-      player.load();
-    }
-  };
-
-  useEffect(() => {
-    props.fetchStream(id);
-    buildPlayer();
-  }, []);
-
-  useEffect(() => {
-    buildPlayer();
-  }, [stream]);
-
-  if (!stream) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Spinner />
-      </div>
-    );
-  }
-
   return (
     <div className="FormContainer pb-10">
-      <video ref={videoRef} className="w-1/2" controls />
       <h1>Stream : {stream.title}</h1>
     </div>
   );
